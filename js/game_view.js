6cccb3268e0829d3a5f420261638191f05f1d809
Abbas <SolokiAbbas@gmail.com>
2018-02-28 15:24:53 -0500

    fixed styling and added background

--- js/game_view.js
+++ js/game_view.js
@@ -41,16 +41,16 @@ class GameView {
       this.game.addGold();
       this.intro = false;
     }
-    this.ctx.font = "26px arial";
+    this.ctx.font = "italic 26pt Calibri";
     this.ctx.fillStyle = 'White';
     this.ctx.fillText("Shop Here", 1045, 30);
-    this.ctx.font = "16px arial";
+    this.ctx.font = "italic 16pt Calibri";
     this.ctx.fillStyle = 'White';
     this.ctx.fillText("Cost: 500 Gold", 1055, 450);
-    this.ctx.font = "20px arial";
+    this.ctx.font = "italic 20pt Calibri";
     this.ctx.fillStyle = 'White';
     this.ctx.fillText("Remove Cannon", 1035, 550);
-    this.ctx.font = "26px arial";
+    this.ctx.font = "italic 26pt Calibri";
     this.ctx.fillStyle = 'White';
     this.ctx.fillText("Start Battle", 1040, 680);
     this.backgroundImage = new Image();
